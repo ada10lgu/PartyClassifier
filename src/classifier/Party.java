e4d0b159d250eadc5bbdb40ab2383b1c3364b269
ada10lgu <ada10lgu@student.lth.se>
2015-04-08 14:20:59 +0200

    More stuff

--- src/classifier/Party.java
+++ src/classifier/Party.java
@@ -2,6 +2,13 @@ package classifier;
 
 public class Party implements Comparable<Party> {
 
+	String party;
+	boolean wheigh;
+	
+	public Party(String party, boolean wheigh) {
+		
+	}
+	
 	@Override
 	public int compareTo(Party arg0) {
 		return 0;
