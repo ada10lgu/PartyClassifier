466b02e0050fb2bb9ee1325336882f5220a4b623
ada10lgu <ada10lgu@student.lth.se>
2015-04-08 14:34:49 +0200

    Snyggade till kod

--- src/classifier/test/TestParty.java
+++ src/classifier/test/TestParty.java
@@ -10,10 +10,10 @@ public class TestParty {
 
 	@Test
 	public void test() {
-		Party p1 = new Party("SD",0.2);
-		Party p2 = new Party("S",0.3);
-		Party p3 = new Party("M",0.2);
-		
+		Party p1 = new Party("SD", 0.2);
+		Party p2 = new Party("S", 0.3);
+		Party p3 = new Party("M", 0.2);
+
 		assertFalse(p1.equals(p2));
 		assertTrue(p1.equals(p1));
 		assertTrue(p1.equals(p3));
@@ -21,7 +21,6 @@ public class TestParty {
 		assertTrue(p3.compareTo(p2) < 0);
 		assertTrue(p2.compareTo(p3) > 0);
 		assertTrue(p3.compareTo(p1) == 0);
-		
 	}
 
 }
