a7bb296396ffcfb035cc1acb083f35fca57e2896
Ivar Bergkvist <ivar.bergkvist@gmail.com>
2015-04-08 14:36:50 +0200

    partyList wip

--- src/classifier/PartyList.java
+++ src/classifier/PartyList.java
@@ -1,9 +1,22 @@
 package classifier;
 
-public class PartyList {
+import java.util.HashMap;
 
+
+
+public class PartyList {
+	private HashMap<String, Party> map;
+	private Party best;
+	
+	public PartyList(){
+		map = new HashMap<String, Party>();
+	}
+	
 	public void add(Party p) {
-		
+		map.put(p.party, p);
+		if(best == null || best.wheigh > p.wheigh){
+			best = p;
+		}
 	}
 	
 	public void multiply(double d) {
