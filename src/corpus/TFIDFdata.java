4c90f8bcd337a197578b916c8e5eda74195d3c25
ada10lgu <ada10lgu@student.lth.se>
2015-04-15 12:42:33 +0200

    Wordcounter and tfidf data done

--- src/corpus/TFIDFdata.java
+++ src/corpus/TFIDFdata.java
@@ -1,19 +1,46 @@
 package corpus;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import csv.CSV;
 
 public class TFIDFdata {
 
-	public TFIDFdata(File f) {
-		
+	private int documents;
+	private HashMap<String, Integer> data;
+
+	public TFIDFdata(File f) throws IOException {
+		data = new HashMap<>();
+
+		CSV csv = new CSV(f);
+
+		ArrayList<ArrayList<String>> rows = csv.getData();
+
+		String n = rows.get(0).get(0);
+		documents = Integer.parseInt(n);
+
+		for (int i = 1; i < rows.size(); i++) {
+			ArrayList<String> row = rows.get(i);
+
+			String word = row.get(0);
+			int ammount = Integer.parseInt(row.get(1));
+			data.put(word, ammount);
+		}
+
 	}
-	
-	public int numberOfPages(){
-		return 0;
+
+	public int numberOfPages() {
+		return documents;
 	}
-	
-	public int wordApparance(String Word) {
-		return 0;
+
+	public int wordApparance(String word) {
+		Integer i = data.get(word);
+		if (i == null)
+			return 0;
+		return i;
 	}
-	
+
 }
--- src/corpus/WordCounter.java
+++ src/corpus/WordCounter.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Set;
 
 import corpus.methods.AmmountMethod;
 import corpus.methods.FrequencyMethod;
@@ -16,19 +16,24 @@ public class WordCounter {
 	public static void main(String[] args) throws IOException {
 
 		
+		System.out.println("Reading data");
 		
 		HashMap<String, ArrayList<Text>> text = getAllPartyTexts();
 
-		HashSet<String> words = getAllWords(text);
-		
 		int documents = 0;
 		for (String key : text.keySet()) {
 			documents += text.get(key).size();
 
 			System.out.println(key + ":\t" + text.get(key).size());
 		}
-		System.out.println(documents);
 
+		
+		System.out.println("Creating tfidf data");
+		Set<String> words = getAllWords(text);
+		createTFIDFData(words, documents, text);
+
+		
+		System.out.println("Calculating methods");
 		HashMap<String, Method> methods = new HashMap<>();
 		methods.put("", new AmmountMethod());
 		methods.put("_p", new FrequencyMethod());
@@ -48,20 +53,43 @@ public class WordCounter {
 
 	}
 
-	private static HashSet<String> getAllWords(HashMap<String, ArrayList<Text>> text) {
-		HashSet<String> data = new HashSet<>();
-		
+	private static void createTFIDFData(Set<String> words, int documents,
+			HashMap<String, ArrayList<Text>> text) throws IOException {
+
+		CSV csv = new CSV(new File("data/tfidf/tfidf.csv"));
+		ArrayList<ArrayList<String>> tfidf = csv.getData();
+		tfidf.clear();
+		ArrayList<String> firstLine = new ArrayList<>();
+		firstLine.add("" + documents);
+		tfidf.add(firstLine);
+
+		for (String s : words) {
+			int i = 0;
+
+			for (String key : text.keySet())
+				for (Text t : text.get(key))
+					if (t.contains(s))
+						i++;
+
+			ArrayList<String> row = new ArrayList<>();
+			row.add(s);
+			row.add("" + i);
+			tfidf.add(row);
+		}
+
+		csv.save();
+
+	}
+
+	private static Set<String> getAllWords(HashMap<String, ArrayList<Text>> text) {
 		Text all = new Text();
-		
+
 		for (String key : text.keySet()) {
 			for (Text t : text.get(key)) {
 				all.addText(t);
 			}
 		}
-		
-		
-		
-		return data;
+		return all.getWords();
 	}
 
 	private static HashMap<String, ArrayList<Text>> getAllPartyTexts()
